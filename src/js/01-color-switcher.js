485e37c463f805d6e5dbbc5b7548edf2cec050ce
Konurbaeva <konurbaevamadina@gmail.com>
2022-02-06 20:11:58 +0100

    init commit color-switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
 
+const startEl = document.querySelector('[data-start]');
+const stopEl = document.querySelector('[data-stop]');
+
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
+const changesBackgroundColorOfBody = function() {
+    const currentColor = `${getRandomHexColor()}`;
+    document.body.style.backgroundColor = currentColor;
+ }
+  
+const onClickChangeColor = () => {
+    setTimeout(changesBackgroundColorOfBody, 1000);
+  };
+  
+
+startEl.addEventListener('click', onClickChangeColor);
+
+stopEl.removeEventListener('click', onClickChangeColor);
