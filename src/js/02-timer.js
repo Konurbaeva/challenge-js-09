38dffa99cb085c2d2335ad24af6fe0314b3993ad
Konurbaeva <konurbaevamadina@gmail.com>
2022-02-09 16:25:37 +0100

    add function convertMs

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,22 +1,47 @@
 import flatpickr from "flatpickr";
 import 'flatpickr/dist/flatpickr.min.css';
 
+let choosenDate = '123';
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+
 const options = {
     enableTime: true,
     time_24hr: true,
     defaultDate: new Date(),
     minuteIncrement: 1,
     onClose(selectedDates) {
-      console.log(selectedDates[0]);
 
-      let choosenDate = selectedDates[0];
+      choosenDate = selectedDates[0];
      
-      console.log('dates: ', choosenDate);
+      console.log(convertMs(choosenDate));
+      console.log('choosenDate: ', choosenDate)
       return choosenDate;
     },
   };
 
 
-
 flatpickr("#datetime-picker", options);
+
+const currentDate = Date.now();
+
+console.log('currentDate', currentDate) // ms 1644419414312#
 
