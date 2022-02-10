7215c451bd22145532ee9ceb29dd7005069542dd
Konurbaeva <konurbaevamadina@gmail.com>
2022-02-10 15:06:30 +0100

    promise first commit

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,39 @@
+
+
+const formEl = document.querySelector('.form')
+const buttonEl = document.querySelector('[type="submit"]');
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
+
   if (shouldResolve) {
-    // Fulfill
+   // resolve(position)
   } else {
-    // Reject
+  //  reject(position)
+
   }
 }
+
+function onPromise(resolve, reject) {
+
+  const DELAY = 1000;
+
+  setTimeout(() => {
+    if (shouldResolve) {
+      resolve(`✅ resolved: ${position}`);
+    }
+
+    reject('❌ rejected');
+  }, DELAY);
+
+}
+
+function onFormNotLoad(event){
+
+  event.preventDefault();
+
+}
+
+
+buttonEl.addEventListener('click', createPromise)
+formEl.addEventListener('submit', onFormNotLoad)
